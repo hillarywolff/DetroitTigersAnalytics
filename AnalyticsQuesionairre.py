b9a913eb0c20bbc97488aee865801543f89cd487
hillarywolff <hwolff@uchicago.edu>
2023-09-26 13:03:52 -0500

    ds_store

---
+++ AnalyticsQuesionairre.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 12:58:22 2023
+
+@author: hillarywolff
+"""
+import os
+directory_path = '/Users/hillarywolff/documents/github/detroittigersanalytics'
+os.chdir(directory_path)
+
+current_directory = os.getcwd()
+print(current_directory)
